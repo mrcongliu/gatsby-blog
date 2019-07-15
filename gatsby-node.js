b983ba0fd251f292459d7595fb2efde399fa3d7c
CongLiu-CN <upcliucong@gmail.com>
2019-07-15 13:14:48 +0800

    build markdown-page path

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+/* Any new field you add goes into the fields property. */
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,14 @@ export default ({ data }) => (
     <div>
       <h1>CongLiu's Thoughts</h1>
       <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
     </div>
   </Layout>
 )
